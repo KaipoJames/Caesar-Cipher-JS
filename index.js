9c7b62b29cea77f7a10d14329ae5d5c65aa94191
Kaipo <kaipojames12@gmail.com>
2021-08-01 22:43:34 -0700

    export functions to from index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import { encrypt } from './src/encrypt.js';
 import { decrypt } from './src/decrypt.js';
 
-const encoded = encrypt("Don't Eat The Rice Unless The Red Flag Is Up", -5);
-console.log(encoded);
-const decoded = decrypt(encoded, 5);
-console.log(decoded);
+// const encoded = encrypt("Don't Eat The Rice Unless The Red Flag Is Up", -5);
+// console.log(encoded);
+// const decoded = decrypt(encoded, 5);
+// console.log(decoded);
+
+module.exports = { encrypt, decrypt };
